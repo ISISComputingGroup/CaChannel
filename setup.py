14ad3e813a5603d8626f15e4ba060806a7f9832b
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2023-02-27 14:21:59 +0000

    Re-enable optimisation

--- setup.py
+++ setup.py
@@ -80,7 +80,8 @@ def create_exension():
             macros += [('_CRT_SECURE_NO_WARNINGS', 'None'), ('EPICS_CALL_DLL', '')]
             if DEBUG:
                 cflags += ['/MDd', '/Od', '/RTCsu']
-            cflags += ['/Z7', '/Od', '/RTCsu']
+            cflags += ['/Z7']
+            #cflags += ['/Od', '/RTCsu'] # use for release debugging
             lflags += ['/DEBUG']
             CMPL = 'msvc'
         if HOSTARCH in ['win32-x86-static', 'windows-x64-static'] or static:
